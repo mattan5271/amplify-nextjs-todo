e3bca1956d8e101dc9ad83e3206db618e89047a4
mattan5271 <mattan.system.engineering@gmail.com>
2022-04-19 10:19:56 +0900

    ⚡️ Todo作成機能追加

---
+++ src/component/Form.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, FC } from "react";
+import { useForm } from "react-hook-form";
+
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+
+const Form = (props) => {
+  const { onSubmit, values } = props;
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setValue,
+    formState: { errors },
+  } = useForm();
+
+  useEffect(() => {
+    if (!values) return;
+    setValue("name", values.name);
+  });
+
+  const handler = (newTodo) => {
+    onSubmit(newTodo);
+    reset();
+  };
+
+  const errorMessage = (errors, field) => {
+    const message = [];
+    if (errors[field]?.type == "required") {
+      message.push("required");
+    }
+    if (errors[field]?.type == "maxLength") {
+      message.push("Exceeded 20 characters");
+    }
+    return message.join(", ");
+  };
+
+  return (
+    <form onSubmit={handleSubmit(handler)}>
+      <Grid container direction="column" spacing={2}>
+        <Grid item md={6}>
+          <TextField
+            label="Name"
+            name="name"
+            fullWidth
+            {...register("name", { required: true, maxLength: 200 })}
+            error={Boolean(errors.name)}
+            helperText={errorMessage(errors, "name")}
+          />
+        </Grid>
+        <Grid item md={6}>
+          <Button type="submit" variant="contained" color="primary">
+            Save
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
+
+export default Form;
